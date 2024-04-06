e25396cdd35764b12350ca96c3599f409446051a
Ben <craviona@gmail.com>
2024-04-05 21:04:04 -0500

    changing the response statement from upload script

--- upload.php
+++ upload.php
@@ -3,5 +3,5 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
 echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
-echo "http://rpi1.local/uploads/" . basename($_FILES["file"]["name"]);
+echo "http://ben-server.local/uploads/" . basename($_FILES["file"]["name"]);
 ?>
