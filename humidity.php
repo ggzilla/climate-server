f609e5850c8274b352c7b2dc18c8a0d4f174869e
ggzilla <45543641+ggzilla@users.noreply.github.com>
2024-04-07 20:42:03 -0500

    trying again to see if that fixed it

--- humidity.php
+++ humidity.php
@@ -3,20 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Index Page</title>
+    <title>Sensor Data</title>
+    <!-- Include jQuery library -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    <h1>Main Index Page</h1>
     <div id="sensor-data"></div>
-
     <script>
         $(document).ready(function() {
             $.get('http://rpi2.local:5000/sensor-data', function(data) {
+                // Parse JSON response
+                var sensorData = JSON.parse(data);
+
+                // Update HTML content with sensor data
                 $('#sensor-data').html(`
-                    <p>Temperature: ${data.temperature}°C</p>
-                    <p>Humidity: ${data.humidity}%</p>
-                    <p>VPD: ${data.vpd} kPa</p>
+                    <p>Temperature: ${sensorData.temperature.toFixed(2)}°C</p>
+                    <p>Humidity: ${sensorData.humidity.toFixed(2)}%</p>
+                    <p>VPD: ${sensorData.vpd.toFixed(2)} kPa</p>
                 `);
             });
         });
